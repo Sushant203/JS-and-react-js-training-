296ae3bce0e354bb34b376e29b901b6e7219c757
Sushant <clashofgames229@gmail.com>
2026-02-09 19:07:57 +0545

    feat:commenced js training DAY 1

---
+++ main.js
@@ -0,0 +1,11 @@
+// var , let  and const in js
+var a = 10;
+var a =20;
+console.log(a)
+// Var is global scoped that can be re-declared
+// Let is block scoped variable that cannot be re-declared
+// let b = 20;
+// let b = 30
+
+const PI = 3.14;
+console.log(PI)
