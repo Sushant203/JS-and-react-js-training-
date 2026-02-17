609363f068d15750fedb5b7b8f0814185833587a
Sushant <clashofgames229@gmail.com>
2026-02-17 10:02:32 +0545

    js dom part 2

---
+++ day5/helloo.py
@@ -0,0 +1,18 @@
+'''name=input("Enter your name: ")
+address=input("Enter your address: ")
+phone=input("enter your number: ")
+print("Your name is ", name)
+print("your address is: ", address)
+print("your number is:", phone)'''
+
+'''
+Q. WAP to take Principle, Time and Rate as input and display the simple interest.
+Q. WAP to find volume of cuboid. 
+Q. WAP to find volume of cube.
+Q, WAP to find area of cirlce.
+
+'''
+p=int(input("enter the priciple: "))
+r=int(input("enter the rate: "))
+t=int(input("enter the time: "))
+print("The simple interest is: ",(p*t*r)/100)
